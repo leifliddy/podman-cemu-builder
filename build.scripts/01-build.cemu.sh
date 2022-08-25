1f9a1322fd37b364d0f4215b98e24124f6d2f1bf
Leif Liddy <leifliddy@users.noreply.github.com>
2022-08-25 05:52:14 +0200

    use -p flag with mkdir
    so it doesn't error if the directory exists

--- build.scripts/01-build.cemu.sh
+++ build.scripts/01-build.cemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-mkdir /root/cemu/build
+mkdir -p /root/cemu/build
 pushd /root/cemu/build
 cmake .. -DCMAKE_BUILD_TYPE=debug -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -G Ninja -DCMAKE_BUILD_WITH_INSTALL_RPATH=ON -DCMAKE_MAKE_PROGRAM=/usr/bin/ninja
 ninja
