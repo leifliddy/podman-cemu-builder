9af1df397cd9cbd7e5f7ef577b69dddef022bae3
Leif Liddy <leif.liddy@gmail.com>
2022-09-01 18:38:53 +0200

    redesign v1

--- podman_variables.py
+++ podman_variables.py
@@ -2,8 +2,25 @@
 
 import os
 
+def create_mounts_dict(host_mount, container_mount, read_only=False, selinux_label=True, selinux_recursive=True):
+# if selinux_label=True the container_file_t label will be applied to the source host directory
+# it can be set to False if the directory exists on an nfs mount and has the nfs_t label set
+# if selinux_recursive=True (and selinux_label=True) the container_file_t label will be recursively applied to the source host directory
+
+    mounts = {
+               'type':              'bind',
+               'source':            host_mount,
+               'target':            container_mount,
+               'read_only':         read_only,
+               'selinux_label':     selinux_label,
+               'selinux_recursive': selinux_recursive
+             }
+
+    return mounts
+
 
 cur_dir                 = os.path.dirname(os.path.realpath(__file__))
+bind_volumes            = []
 
 image_name              = 'cemu_build_env'
 container_name          = 'cemu_builder'
@@ -15,12 +32,8 @@ scripts_dir_container   = '/root/scripts'
 output_dir_host         = f'{cur_dir}/output'
 output_dir_container    = '/output'
 
-# used in debug output
-podman_vol_str          = f'-v {scripts_dir_host}:{scripts_dir_container} -v {output_dir_host}:{output_dir_container}'
+bind_volumes.append(create_mounts_dict(scripts_dir_host, scripts_dir_container))
+bind_volumes.append(create_mounts_dict(output_dir_host, output_dir_container))
 
-# ensure bind mounted directories have the container_file_t label set
-mount_dirs_selinux = [scripts_dir_host, output_dir_host]
-
-bind_volumes = []
-bind_volumes.append({'source': f'{scripts_dir_host}', 'target': f'{scripts_dir_container}', 'type': 'bind'})
-bind_volumes.append({'source': f'{output_dir_host}', 'target': f'{output_dir_container}', 'type': 'bind'})
+# set privileged mode
+privileged = False
