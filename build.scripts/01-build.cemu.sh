6564a070e6c8570ccb5f6a0cf42bddbbba734659
Leif Liddy <leif.liddy@gmail.com>
2022-10-05 07:23:56 +0200

    added git pull functionality

--- build.scripts/01-build.cemu.sh
+++ build.scripts/01-build.cemu.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
  
+# perform git pull
+pushd /root/cemu
+git pull --recurse-submodules
+popd 
+
+# build cemu
 mkdir -p /root/cemu/build
 pushd /root/cemu/build
 cmake .. -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -G Ninja
