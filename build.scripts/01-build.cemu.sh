d209771d41e82ebd31135202859a332a90b28fba
Leif Liddy <leif.liddy@gmail.com>
2022-10-07 22:36:33 +0200

    slightly altered build script

--- build.scripts/01-build.cemu.sh
+++ build.scripts/01-build.cemu.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
- 
+
+set -e
+
 # perform git pull
 pushd /root/cemu
 git pull --recurse-submodules
-popd 
+popd
+
+# remove build directory
+[[ -d /root/cemu/build ]] && rm -rf /root/cemu/build
 
 # build cemu
-mkdir -p /root/cemu/build
-pushd /root/cemu/build
-cmake .. -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -G Ninja
-cmake --build .
+pushd /root/cemu
+cmake -S . -B build -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -G Ninja
+cmake --build build
 popd
-strip /root/cemu/bin/Cemu
+
+strip /root/cemu/bin/Cemu_release
 rsync -vr --delete --exclude='.gitignore' /root/cemu/bin/ /output/
