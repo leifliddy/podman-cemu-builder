beea0fd34b7f4625aab137ee420c665c3804b22e
Leif Liddy <leif.liddy@gmail.com>
2022-12-24 17:55:38 +0100

    rework build script

--- Containerfile
+++ Containerfile
@@ -6,8 +6,7 @@ RUN dnf update -y &&\
     dnf install -y clang cmake cubeb-devel findutils freeglut-devel git gtk3-devel kernel-headers libgcrypt-devel libsecret-devel nasm ninja-build perl-FindBin perl-File-Compare perl-File-Copy perl-IPC-Cmd rsync systemd-devel vcpkg vim-enhanced wget zip zlib-devel &&\
     find /root/ -type f | egrep 'anaconda-ks.cfg|anaconda-post-nochroot.log|anaconda-post.log|original-ks.cfg' | xargs rm -f &&\
     echo 'defaultyes=True' >> /etc/dnf/dnf.conf &&\
-    mkdir /root/.bashrc.d /root/cemu &&\
-    git clone --recursive https://github.com/cemu-project/Cemu /root/cemu
+    mkdir /root/.bashrc.d
 
 # set login directory
 WORKDIR /root
--- build.scripts/01-build.cemu.sh
+++ build.scripts/01-build.cemu.sh
@@ -2,19 +2,34 @@
 
 set -e
 
+cemu_dir='/root/cemu'
+cemu_git_url='https://github.com/cemu-project/Cemu'
+
+while getopts :r arg
+do
+    case "${arg}" in
+        r) rebuild=true ;;
+    esac
+done
+
+[[ $rebuild = true ]] && [[ -d $cemu_dir ]] && rm -rf $cemu_dir
+[[ ! -d $cemu_dir ]] && mkdir $cemu_dir && rebuild=true
+
+[[ $rebuild = true ]] && git clone --recursive $cemu_git_url $cemu_dir
+
 # perform git pull
-pushd /root/cemu
+pushd $cemu_dir
 git pull --recurse-submodules
 popd
 
 # remove build directory
-[[ -d /root/cemu/build ]] && rm -rf /root/cemu/build
+[[ -d $cemu_dir/build ]] && rm -rf $cemu_dir/build
 
 # build cemu
-pushd /root/cemu
+pushd $cemu_dir
 cmake -S . -B build -DCMAKE_BUILD_TYPE=release -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++ -G Ninja
 cmake --build build
 popd
 
 strip /root/cemu/bin/Cemu_release
-rsync -vr --delete --exclude='.gitignore' /root/cemu/bin/ /output/
+rsync -vr --delete --exclude='.gitignore' $cemu_dir/bin/ /output/
